153b7ee8b474cb3bfa0c059101303fd1db7bb08b
johnmayerbass <berenbaum4@gmail.com>
2025-01-18 10:35:55 +0900

    Update site.config.js

--- site.config.js
+++ site.config.js
@@ -57,7 +57,8 @@ const CONFIG = {
   utterances: {
     enable: true,
     config: {
-      repo: process.env.NEXT_PUBLIC_UTTERANCES_REPO || "",
+      // repo: process.env.NEXT_PUBLIC_UTTERANCES_REPO || "",
+      repo: "johnmayerbass/diary"
       "issue-term": "og:title",
       label: "💬 Utterances",
     },
